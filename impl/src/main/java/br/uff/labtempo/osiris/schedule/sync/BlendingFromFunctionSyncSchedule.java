2abe953ca3c037e9019ce5f83e66b946a0236c4b
aghigo <andreghigo@id.uff.br>
2017-06-17 23:12:11 -0300

    Chanding call interval

--- impl/src/main/java/br/uff/labtempo/osiris/schedule/sync/BlendingFromFunctionSyncSchedule.java
+++ impl/src/main/java/br/uff/labtempo/osiris/schedule/sync/BlendingFromFunctionSyncSchedule.java
@@ -26,7 +26,7 @@ import java.util.List;
 @Slf4j
 public class BlendingFromFunctionSyncSchedule {
 
-    private final long defaultCallIntervalInMillis = 500;
+    private final long defaultCallIntervalInMillis = 2000;
 
     @Autowired
     private CompositeRepository compositeRepository;
